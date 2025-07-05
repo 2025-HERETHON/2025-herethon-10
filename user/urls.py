43ca5bd4c4cde0fb1e6d99f13a32375e57dc61ce
jwdebbie <leejoowon09@gmail.com>
2025-07-06 07:34:09 +0900

    Feat : url 추가

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from .views import login_view
 
 app_name = 'user'
 
 urlpatterns = [
     path('signup/', views.signup_view, name='signup'),
+    path('login/', login_view, name='login'),
 ]
