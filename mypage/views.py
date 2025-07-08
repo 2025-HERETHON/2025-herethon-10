83cf3ac4abe85adf61e4dafcc77847b2e60b7298
jwdebbie <leejoowon09@gmail.com>
2025-07-09 08:38:35 +0900

    Feat : 뷰 작성

--- mypage/views.py
+++ mypage/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from user.models import IndependencePlan
+from .forms import UserUpdateForm, IndependencePlanUpdateForm
+
+
+@login_required
+def profile_edit(request):
+    user = request.user
+    plan = get_object_or_404(IndependencePlan, user=user)
+
+    if request.method == 'POST':
+        user_form = UserUpdateForm(request.POST, instance=user)
+        plan_form = IndependencePlanUpdateForm(request.POST, instance=plan)
+
+        if user_form.is_valid() and plan_form.is_valid():
+            user_form.save()
+            plan_form.save()
+            return redirect('mypage:profile_edit')
+    else:
+        user_form = UserUpdateForm(instance=user)
+        plan_form = IndependencePlanUpdateForm(instance=plan)
+
+    return render(request, 'user_update.html', {
+        'user_form': user_form,
+        'plan_form': plan_form,
+    })
