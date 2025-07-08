f45480c6f6d6d39db351ddfebeff13cdd208367f
jwdebbie <leejoowon09@gmail.com>
2025-07-08 11:52:12 +0900

    Feat : 뷰 함수 작성

--- home/views.py
+++ home/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+def landing_view(request):
+    if request.user.is_authenticated:
+        return redirect('home')  # 로그인 돼 있으면 홈으로
+    return render(request, 'home/landing.html')  # 아니면 랜딩 페이지 보여줌
+
+def home_view(request):
+    if not request.user.is_authenticated:
+        return redirect('landing')  # 로그인 안 돼 있으면 랜딩으로
+    return render(request, 'home/home.html', {
+        'user': request.user,
+    })
