e8fe8e34151e06c37413e716f9caa0e563b4cb6d
Son <suruna1026@gmail.com>
2025-07-09 09:39:27 +0900

    feat : contract_page_profile

--- independa/views/moving_veiws.py
+++ independa/views/moving_veiws.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 
 from independa.models import CheckAll
+from user.models import IndependencePlan
 
 
 def moving_checklist_edit_view(request):
     check=CheckAll.objects.get(user_id=request.user.id)
-    return render(request, 'test_checklist_move.html',{
+    independenceplan = IndependencePlan.objects.filter(user_id=request.user.id).first()
+    
+    context = {
         # 'form': form,
-        'check':check
-    } )
+        'check':check,
+        'independenceplan':independenceplan,
+    }
+    return render(request, 'test_checklist_move.html', context )
