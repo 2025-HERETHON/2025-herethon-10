1eb933fc5859a5da6e2998740d760571caa3b73b
jwdebbie <leejoowon09@gmail.com>
2025-07-09 10:20:47 +0900

    Feat : mypage 회원정보 수정 구현

--- mypage/forms.py
+++ mypage/forms.py
@@ -1,22 +1,40 @@
 from django import forms
 from user.models import User, IndependencePlan
 
+MOVE_DATE_CHOICES = [
+    ('2025-07', '2025년 7월'),
+    ('2025-08', '2025년 8월'),
+    ('2025-09', '2025년 9월'),
+]
 
 class UserUpdateForm(forms.ModelForm):
+    profile_image = forms.ImageField(required=False)
+
+    
+    GENDER_CHOICES = [
+        ('여성', '여성'),
+        ('남성', '남성'),
+    ]
+
+    gender = forms.ChoiceField(
+        choices=GENDER_CHOICES,
+        widget=forms.RadioSelect
+    )
+    
     birth_date = forms.DateField(
         widget=forms.DateInput(attrs={'type': 'date'})
     )
-    gender = forms.ChoiceField(
-        choices=User.GENDER_CHOICES,
-        widget=forms.RadioSelect
-    )
-
+    
     class Meta:
         model = User
-        fields = ['name', 'email', 'gender', 'birth_date', 'job']
+        fields = ['name', 'email', 'gender', 'birth_date', 'job', 'profile_image']
 
 
 class IndependencePlanUpdateForm(forms.ModelForm):
+    # 숨김 필드 + 필수 아님 (JS로 채움)
+    area_si = forms.CharField(widget=forms.HiddenInput())
+    area_sgg = forms.CharField(widget=forms.HiddenInput())
+
     class Meta:
         model = IndependencePlan
         exclude = ['user']
--- mypage/views.py
+++ mypage/views.py
@@ -1,29 +1,25 @@
-from django.shortcuts import render
-
-from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.decorators import login_required
-from user.models import IndependencePlan
+from django.shortcuts import render, redirect
+from user.models import User, IndependencePlan
 from .forms import UserUpdateForm, IndependencePlanUpdateForm
 
-
-@login_required
 def profile_edit(request):
     user = request.user
-    plan = get_object_or_404(IndependencePlan, user=user)
+    plan = IndependencePlan.objects.get(user=user)
 
     if request.method == 'POST':
-        user_form = UserUpdateForm(request.POST, instance=user)
+        user_form = UserUpdateForm(request.POST, request.FILES, instance=user)
         plan_form = IndependencePlanUpdateForm(request.POST, instance=plan)
 
         if user_form.is_valid() and plan_form.is_valid():
             user_form.save()
             plan_form.save()
             return redirect('mypage:profile_edit')
+
     else:
         user_form = UserUpdateForm(instance=user)
         plan_form = IndependencePlanUpdateForm(instance=plan)
 
-    return render(request, 'user_update.html', {
+    return render(request, 'test_user_update.html', {
         'user_form': user_form,
         'plan_form': plan_form,
     })
